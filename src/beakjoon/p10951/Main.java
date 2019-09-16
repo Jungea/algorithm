d8e6c3651341f56477b0a9e944d81875b702610e
Jungea <jungea2241@gmail.com>
2019-09-16 16:53:32 +0900

    단계별로 풀어보기- while문

---
+++ src/beakjoon/p10951/Main.java
@@ -0,0 +1,24 @@
+/*
+ * 작성자: 정은애
+ * 작성일: 2019.09.15.
+ * 백준 10951. A+B - 4
+ */
+
+package beakjoon.p10951;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		while (scan.hasNext()) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			System.out.println(a + b);
+		}
+
+	}
+
+}
