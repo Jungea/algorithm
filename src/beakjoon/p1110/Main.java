3c228859b13b5b6125ace04ceb16bdc4e85de38e
Jungea <jungea2241@gmail.com>
2019-09-16 17:34:19 +0900

    [bj1110]한자리 입력 처리

--- src/beakjoon/p1110/Main.java
+++ src/beakjoon/p1110/Main.java
@@ -2,6 +2,7 @@
  * 작성자: 정은애
  * 작성일: 2019.09.16.
  * 백준 1110. 더하기 사이클
+ * 한자리 수 입력 처리 수정 
  */
 
 package beakjoon.p1110;
@@ -13,24 +14,21 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try (Scanner scan = new Scanner(System.in)) {
-			String n = scan.nextLine();
-			String k = new String(n);
+			int n = Integer.parseInt(scan.nextLine());
+
+			int k = n;
+
 			int count = 0;
+
 			do {
-				if (k.length() == 1)
-					k = k + "" + k;
-				else {
-					int a = k.charAt(0) - 48;
-					int b = k.charAt(1) - 48;
-					String c = Integer.toString(a + b);
-					if (c.length() == 1)
-						k = b + "" + c;
-					else
-						k = b + "" + (c.charAt(1) - 48);
-				}
+				int a = k / 10; // 십의 자리
+				int b = k % 10; // 일의 자리
+				int c = (a + b) % 10;
+
+				k = b * 10 + c;
 
 				count++;
-			} while (n.equals(k) == false);
+			} while (n != k);
 
 			System.out.println(count);
 		}
