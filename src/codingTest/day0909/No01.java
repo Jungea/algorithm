9b93e06f3f7f1190a737985a97d90c2535d64ee7
Jungea <jungea2241@gmail.com>
2019-10-24 17:35:22 +0900

    단계별로 풀어보기- 수학2 (소수)

--- src/codingTest/day0909/No01.java
+++ src/codingTest/day0909/No01.java
@@ -7,8 +7,10 @@
 package codingTest.day0909;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class No01 {
@@ -19,6 +21,12 @@ public class No01 {
 		String s = reader.readLine();
 		StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
 		String opt = tokenizer.nextToken();
+
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		writer.write(s + "\n");
+
+		writer.flush();
+		writer.close();
 	}
 
 }
